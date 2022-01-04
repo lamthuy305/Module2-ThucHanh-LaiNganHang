52d76cda941e4caddab6a47ea54ab919571a6bcf
lamthuy305 <lamthuy305@gmail.com>
2022-01-04 16:53:38 +0700

    ok

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,21 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập số tiền gốc: ");
+        double money = scanner.nextDouble();
+        System.out.println("Nhập số tháng gửi: ");
+        int month = scanner.nextInt();
+        System.out.println("Nhập lãi suất tháng(%): ");
+        double laiSuat = scanner.nextDouble();
+        double lai = 0;
+        lai = (money * month * laiSuat) / 100;
+        System.out.println("Tổng tiền lãi là: " + lai);
+
+    }
+}
